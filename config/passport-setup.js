be167f9998264502dc1fc5d7eac672dedbbadbe0
Guilherme Miguel Roque <62621598+guimroque@users.noreply.github.com>
2020-09-02 18:51:14 -0300

    Update passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -11,8 +11,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 passport.use(new GoogleStrategy({//configurando passport com dados da API do google
-    clientID: '701663066930-1fb9a9qt6250ubsmadltukvv3c5raokt.apps.googleusercontent.com',
-    clientSecret: 'wPzKOWkvAld2xn4hU431Da77',
+    clientID: '********************************',
+    clientSecret: '*************************',
     callbackURL: "http://localhost:8081/google/callback"
   },//função que retorna dados do usuario pelo profille
   function(accessToken, refreshToken, profile, done) {
@@ -21,8 +21,8 @@ passport.use(new GoogleStrategy({//configurando passport com dados da API do goo
 ));
 
 passport.use(new FacebookStrategy({
-  clientID: '760626761395615',
-  clientSecret: '4a474323e065b14f864e816e9f37e2ef',
+  clientID: '*************',
+  clientSecret: '*****************',
   callbackURL: "http://localhost:8081/facebook/callback"
 },
 function(accessToken, refreshToken, profile, done) {
